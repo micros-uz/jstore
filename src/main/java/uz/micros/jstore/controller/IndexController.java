4af302159d37f133105cbbb6e62416642d4027da
Artem <mmm@mail.mail>
2014-05-06 19:58:35 +0500

    Added: prepar for 2 lesson - Minimal Spring Java Config without views. Work on IDEA Tomcats (7 and 8), Jetty (9.1.5) and with Tomcat7 and Jetty plugins. Standalone Jetty requires ASM 5.0.2 in lib

---
+++ src/main/java/uz/micros/jstore/controller/IndexController.java
@@ -0,0 +1,17 @@
+package uz.micros.jstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String showIndex() {
+        return "<h2>Hello world</h2>";
+    }
+
+}
