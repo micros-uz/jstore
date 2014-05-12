7ef3263db16f332586020683c8b222054a765df8
micros-uz <mail@m.e>
2014-05-13 00:35:15 +0500

    Added: Simple JSP View

--- src/main/java/uz/micros/jstore/controller/IndexController.java
+++ src/main/java/uz/micros/jstore/controller/IndexController.java
@@ -11,7 +11,13 @@ public class IndexController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String showIndex() {
-        return "<h2>Hello world</h2>";
+        return "<h2>Hello world</h2>" +
+                "" +
+                "<a href=\"home\">Home</a>";
     }
 
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
 }
