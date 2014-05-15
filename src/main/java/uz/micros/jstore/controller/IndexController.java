ef2ace2273acaff54a75e204581ed801dd4d954d
Artem <mmm@mail.mail>
2014-05-15 17:29:37 +0500

    Added: Simple Form Post - use HTML raw form (ATTENTION - save jstore.iml before pull)

--- src/main/java/uz/micros/jstore/controller/IndexController.java
+++ src/main/java/uz/micros/jstore/controller/IndexController.java
@@ -3,10 +3,12 @@ package uz.micros.jstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import uz.micros.jstore.entity.store.Author;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,18 +24,35 @@ public class IndexController {
     }
 
     @RequestMapping("/home")
-    public ModelAndView home(){
+    public ModelAndView home(HttpSession session) {
         Author a = new Author();
         a.setName("Akbar");
 
-        List<Author> list = new ArrayList<>();
-        list.add(a);
-        list.add(a);
-        list.add(a);
+        List<Author> list;
+        if (session.getAttribute("authors") == null) {
+            list = new ArrayList<>();
+            list.add(a);
+            list.add(a);
+            list.add(a);
+            session.setAttribute("authors", list);
+        } else
+            list = (List<Author>) session.getAttribute("authors");
 
         return new ModelAndView("home")
                 .addObject("name", "John")
                 .addObject("author", a)
                 .addObject("authors", list);
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(@RequestParam String title, HttpSession session) {
+        List<Author> list = (List<Author>) session.getAttribute("authors");
+
+        Author a = new Author();
+        a.setName(title);
+
+        list.add(a);
+
+        return "redirect:/";
     }
 }
