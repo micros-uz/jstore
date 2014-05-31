eba2b09387794ed56584d5c4238bd648fef49d14
Artem <mmm@mail.mail>
2014-05-31 16:23:46 +0500

    Added: Comment add form + Post new comment to server (without saving)

--- src/main/java/uz/micros/jstore/controller/blog/PostController.java
+++ src/main/java/uz/micros/jstore/controller/blog/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import uz.micros.jstore.entity.blog.Comment;
 import uz.micros.jstore.entity.blog.Post;
 import uz.micros.jstore.service.blog.PostService;
 
@@ -22,6 +23,7 @@ public class PostController {
         Post post = service.get(id);
 
         return new ModelAndView("blog/post")
-                .addObject("post", post);
+                .addObject("post", post)
+                .addObject("newComment", new Comment());
     }
 }
--- src/main/java/uz/micros/jstore/service/blog/PostService.java
+++ src/main/java/uz/micros/jstore/service/blog/PostService.java
@@ -3,7 +3,13 @@ package uz.micros.jstore.service.blog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uz.micros.jstore.entity.blog.Blog;
+import uz.micros.jstore.entity.blog.Comment;
 import uz.micros.jstore.entity.blog.Post;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -17,6 +23,19 @@ public class PostService {
 
         for(Post post : blog.getPosts()){
             if (post.getId() == id){
+                Comment comment = new Comment();
+                comment.setDate(new Date());
+                comment.setAuthor("Davron");
+                comment.setText("Urtoqlar! Let's work!");
+
+                List<Comment> list = new ArrayList<Comment>();
+                list.add(comment);
+                list.add(comment);
+                list.add(comment);
+                list.add(comment);
+
+                post.setComments(list);
+
                 return post;
             }
         }
