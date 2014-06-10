f8d9d92e07593dcd55272b1f07a1c2672b9800b4
Artem <mmm@mail.mail>
2014-06-10 17:45:45 +0500

    Added: DAL - data access sublayer is more universal (DbManager) - returns List<Map<String, Object>>

--- src/main/java/uz/micros/jstore/repository/PostRepository.java
+++ src/main/java/uz/micros/jstore/repository/PostRepository.java
@@ -1,7 +1,15 @@
 package uz.micros.jstore.repository;
 
+import org.springframework.stereotype.Service;
 import uz.micros.jstore.entity.blog.Post;
+import uz.micros.jstore.util.DbManager;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class PostRepository {
     public void addPost(Post post){
 
@@ -17,5 +25,23 @@ public class PostRepository {
 
     public void deletePost(int id){
 
+    }
+
+    public List<Post> getPosts() {
+        List<Post> res = new ArrayList<>();
+        List<Map<String, Object>> list = DbManager.runQuery("select * from \"Posts\"");
+
+        for(Map<String, Object> item : list){
+            Post p = new Post();
+
+            p.setId((Integer)item.get("Id"));
+            p.setSubject((String) item.get("Subject"));
+            p.setDate((Date) item.get("Date"));
+            p.setText((String) item.get("Text"));
+
+            res.add(p);
+        }
+
+        return res;
     }
 }
