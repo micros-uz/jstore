cf46495bafb98c05d56f654d9f4b66de764deb1a
Artem <mmm@mail.mail>
2014-07-09 18:23:19 +0500

    Added: internationalization and RU localization

--- src/main/java/uz/micros/jstore/config/WebMvcConfig.java
+++ src/main/java/uz/micros/jstore/config/WebMvcConfig.java
@@ -1,18 +1,25 @@
 package uz.micros.jstore.config;
 
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import uz.micros.jstore.controller.store.AuthorFormatter;
 import uz.micros.jstore.controller.store.GenreFormatter;
+
+import java.util.Locale;
 
 
 @Configuration
@@ -25,9 +32,24 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver lr = new SessionLocaleResolver();
+        lr.setDefaultLocale(Locale.ENGLISH);
+        return lr;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //registry.addInterceptor(new Interceptor());
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor=new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
     }
 
     @Override
@@ -50,6 +72,20 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource res = new ReloadableResourceBundleMessageSource();
+        res.setBasename("/WEB-INF/i18n/messages");
+        res.setDefaultEncoding("UTF-8");
+        res.setFallbackToSystemLocale(false);
+        //messageSource.setFileEncodings();
+        res.setCacheSeconds(5);
+        // if true, the key of the message will be displayed if the key is not
+        // found, instead of throwing a NoSuchMessageException
+        res.setUseCodeAsDefaultMessage(false);
+        return res;
     }
 }
 
